b3908d53b5cc0c52899dfaaa5070f236aa5a97ca
aaronbiscotti <thesoupmanguy@gmail.com>
2026-02-21 09:39:22 -0700

    image migration

--- backend/alembic/versions/20260221_0009_cover_image_to_db.py
+++ backend/alembic/versions/20260221_0009_cover_image_to_db.py
@@ -1,15 +1,15 @@
 """store cover image data in database as BYTEA
 
-Revision ID: 20260221_0007
-Revises: 20260221_0006
+Revision ID: 20260221_0009
+Revises: 20260221_0008
 Create Date: 2026-02-21
 """
 
 from alembic import op
 import sqlalchemy as sa
 
-revision = "20260221_0007"
-down_revision = "20260221_0006"
+revision = "20260221_0009"
+down_revision = "20260221_0008"
 branch_labels = None
 depends_on = None
 
